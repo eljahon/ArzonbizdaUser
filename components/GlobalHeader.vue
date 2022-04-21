4865449db6473e4744698fb040b1fd4fa6821127
Abdulvohid Karimov <abdulvohid.karimjonov.97@gmail.com>
2022-04-21 12:10:03 +0500

    finished theme js

--- components/GlobalHeader.vue
+++ components/GlobalHeader.vue
@@ -1,13 +1,42 @@
 <template>
-  <div>
-    <h3>test header</h3>
+  <div class="container">
+    <h2>asdasdas</h2>
+    <!-- <CButton variant-color="green">Button</CButton> -->
+    <CThemeProvider>
+      <c-button
+        variant="primary"
+        size="md"
+        height="50px"
+        width="250px"
+        bg-color="color.TextColor2"
+        border="none"
+        _focus="none"
+        cursor="pointer"
+      >
+        Button
+      </c-button>
+    </CThemeProvider>
   </div>
 </template>
 
-<script>
+<script lang="js">
+
+import { CThemeProvider,  CButton } from '@chakra-ui/vue'
+
 export default {
+
   name: 'TestHeader',
+  components: {
+    CThemeProvider,
+    CButton
+  },
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import '@/assets/variables.scss';
+
+h2 {
+  color: $RouteColor;
+}
+</style>
