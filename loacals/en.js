2d1f757392a8f22c4e649206d26cee7d78e5a59d
unknown <sarvarabduganiyev636@gmail.com>
2022-04-26 12:25:40 +0500

    finished Banner, description, blogs

--- loacals/en.js
+++ loacals/en.js
@@ -2,4 +2,14 @@ export const en = {
   error: 'error',
   populargoods: 'Popular goods',
   compare: 'Compare',
+
+  blogText:
+    'The comfortable life of any person and convenient life cannot be imagined without modern technology. Work and personal issues - in a smartphone, favorite films and TV shows - a TV, cleaning - a vacuum cleaner, cooking - a stove, oven and many smaller, but no less useful devices. In addition, technologies are developing, and therefore electronics require regular updates, I want to test new models, or simply there is not enough power of existing devices. In this regard, it is useful to have a reliable place to buy household appliances. If you have not yet found one for yourself, we invite you to stay on our website for a few minutes. We assure you that you have not wasted your time.',
+  shoptext: 'Internet shop - ArzonBizda',
+  shoptexttitle: 'Convenient online hypermarket for shopping',
+  shoptexttitle2: 'Shopping in the online store - fast, convenient, safe',
+  blogText2:
+    'Arzonbizda.uz – this is a company that has been on the market since 2022, but the experience of employees in this field reaches 16 years. We work with good brands of equipment of different price segments and we can offer each client an option for his budget. The convenience of online shopping for you is accompanied by the financial benefits of this format of work. Save and buy reliable products in a few clicks. We will be happy to advise.',
+  newBlogWord: 'New Blogs',
+  newBlogWordText: 'Great phones for neat guys',
 }
--- loacals/uz.js
+++ loacals/uz.js
@@ -4,8 +4,8 @@ export const uz = {
   blogText:
     "Har qanday insonning farovon hayoti va qulay hayotini zamonaviy texnologiyalarsiz tasavvur etib bo'lmaydi. Ish va shaxsiy muammolar - smartfonda, sevimli filmlar va teleko'rsatuvlar - televizor, tozalash - changyutgich, pishirish - pechka, pech va ko'plab kichikroq, ammo foydali qurilmalar. Bundan tashqari, texnologiyalar rivojlanmoqda va shuning uchun elektronika muntazam yangilanishlarni talab qiladi, men yangi modellarni sinab ko'rmoqchiman yoki shunchaki mavjud qurilmalarning kuchi etarli emas. Shu nuqtai nazardan, maishiy texnika sotib olish uchun ishonchli joyga ega bo'lish foydalidir. Agar siz hali o'zingiz uchun birini topa olmagan bo'lsangiz, biz sizni veb-saytimizda bir necha daqiqa qolishni taklif qilamiz. Sizni ishontirib aytamizki, vaqtingizni behuda o'tkazmadingiz.",
   shoptext: "Internet-do'kon - ArzonBizda",
-  shoptextSarlavha: 'Xarid qilish uchun qulay onlayn gipermarket',
-  shoptextSarlavha2: "Onlayn do'konda xarid qilish - tez, qulay, xavfsiz",
+  shoptexttitle: 'Xarid qilish uchun qulay onlayn gipermarket',
+  shoptexttitle2: "Onlayn do'konda xarid qilish - tez, qulay, xavfsiz",
   blogText2:
     "Arzonbizda.uz - 2022 yildan beri bozorda faoliyat yuritayotgan kompaniya, ammo bu sohadagi xodimlarning tajribasi 16 yilga etadi. Biz har xil narx segmentlarining yaxshi brendlari bilan ishlaymiz va har bir mijozga o'z byudjetiga mos variantni taklif qilishimiz mumkin. Siz uchun onlayn xarid qilish qulayligi ushbu ish formatining moliyaviy afzalliklari bilan birga keladi. Bir necha marta bosish orqali ishonchli mahsulotlarni saqlang va sotib oling. Biz maslahat berishdan xursand bo'lamiz.",
   newBlogWord: 'Yangi Bloglar',
