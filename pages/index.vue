28cefc784f24b5b1f8f7cdc5d09c2e102ca2d33c
eljahon <neljahon@gmail.com>
2022-05-05 10:47:10 +0500

    global store add

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,31 @@
 <template>
   <div>
     <Home />
+<!--    <button @click='() => $store.dispatch("coutn", 3)'>ok</button>-->
   </div>
 </template>
 <script lang="js">
-
 export default {
-
+  // async asyncData({ $axios }) {
+  //   const data  = await $axios.get("/product/home");
+  //   console.log(data.data)
+  //   return {data:data.data.data.products};
+  // },
 name:"IndexComponent",
 
 components: {
 },
+  computed: {
+  },
+  mounted() {
+  // this.feat()
+  },
+  methods: {
+   // async feat ()  {
+   //   const data  = await this.$axios.get("/product/home");
+   //   console.log(data.data.data.products)
+   // }
+  }
+
 }
 </script>
