c6429eea69e2c3303ea7dbe1db22febec8f8ae32
unknown <sarvarabduganiyev636@gmail.com>
2022-05-06 16:35:39 +0500

    finished translate blog-disc

--- locales/en.js
+++ locales/en.js
@@ -50,4 +50,6 @@ export const en = {
   more: 'More',
   lorem:
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dui amet tellusmassa non tortor orci, quam id ut. Fames eleifend viverra egestas donecnon egestas erat. Eu, et pulvinar dolor facilisis malesuada. Lectus tellusvestibulum tempor est vestibulum sit sed tristique. Non vitae sed sed volutpat mauris nulla. Risus, curabitur neque risus rhoncus. Viverra ininteger tincidunt orci at id odio montes. Amet volutpat eu, sed egestas volutpat maecenas ultricies semper. Ut elit cursus morbi id faucibus.',
+  forguys: 'Great phones for neat guys',
+  ivan: 'Ivan Ivanovich Ivanov.',
 }
