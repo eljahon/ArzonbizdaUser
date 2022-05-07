39836c0d03662eb339d95d73c97958d13a103c88
Sadirov Izzatillo <sadirovizzatillo3@gmail.com>
2022-05-07 12:22:47 +0500

    all slider errors and button slider fixed

--- pages/user.vue
+++ pages/user.vue
@@ -1,48 +1,15 @@
 <script>
-import { CFlex, CBox } from '@chakra-ui/vue'
-import CategoryFilter from '../my-components/categories-component/CategoryFilter.vue'
-import CategoryMainWrapper from '../my-components/categories-component/CategoryMainWrapper.vue'
-import BannerImage from '../my-components/bannerImgBox/BannerImg.vue'
+import ProductSliderVue from '~/my-components/product-slider/ProductSlider.vue'
 export default {
   name: 'UserComponent',
   components: {
-    CategoryFilter,
-    BannerImage,
-    CategoryMainWrapper,
-    CFlex,
-    CBox,
-  },
-  data() {
-    return {
-      switched: false,
-    }
-  },
-  methods: {
-    done() {
-      this.switched = !this.switched
-    },
+    ProductSliderVue,
   },
 }
 </script>
 
 <template>
   <div>
-    <CFlex>
-      <CBox v-if="switched">
-        <CategoryFilter class="category" />
-      </CBox>
-      <CBox>
-        <CFlex direction="column">
-          <BannerImage mt="88px" image-path="banner1.png" />
-          <CategoryMainWrapper @switch="done" />
-        </CFlex>
-      </CBox>
-    </CFlex>
+    <ProductSliderVue />
   </div>
 </template>
-
-<style scoped>
-.category {
-  margin-right: 48px;
-}
-</style>
