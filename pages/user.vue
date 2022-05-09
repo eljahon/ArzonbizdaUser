55d2c6705e63cd4d4537ecc8f122d64a186a2572
Sadirov Izzatillo <sadirovizzatillo3@gmail.com>
2022-05-09 16:04:16 +0500

    fix error

--- pages/user.vue
+++ pages/user.vue
@@ -1,15 +1,18 @@
 <script>
 import CategoryMain from '~/my-components/categories-component/CategoryMain.vue'
+// import ProductSlider from '../my-components/product-slider/ProductSlider.vue'
 export default {
   name: 'UserComponent',
   components: {
     CategoryMain,
+    // ProductSlider,
   },
 }
 </script>
 
 <template>
   <div>
+    <!-- <ProductSlider /> -->
     <CategoryMain />
   </div>
 </template>
