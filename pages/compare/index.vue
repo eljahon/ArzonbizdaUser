5c43f235dc88e0367984f4d7c2d8d09180a895ce
Muslihiddin <muslihiddin97@gmail.com>
2022-05-10 11:49:20 +0500

    Component-stucture-done

--- pages/compare/index.vue
+++ pages/compare/index.vue
@@ -2,10 +2,18 @@
 import { CSimpleGrid } from '@chakra-ui/vue'
 import CompareProduct from '~/my-components/comparing-component/CompareProduct.vue'
 import CompareProductMobile from '~/my-components/comparing-component/CompareProductMobile.vue'
+import CompareTab from '~/my-components/compare-tabs/CompareTab.vue'
+import ProductSlider from '~/my-components/product-slider/ProductSlider.vue'
 
 export default {
   name: 'ComparePage',
-  components: { CompareProduct, CSimpleGrid, CompareProductMobile },
+  components: {
+    CompareProduct,
+    CSimpleGrid,
+    CompareProductMobile,
+    CompareTab,
+    ProductSlider,
+  },
 }
 </script>
 <template>
@@ -14,10 +22,14 @@ export default {
     :spacing="['10px', '12px', '16px', '30px', '30px', '30px']"
     mt="40px"
   >
+    <product-slider />
+    <product-slider />
     <compare-product />
     <compare-product />
     <compare-product-mobile />
     <compare-product-mobile />
+    <compare-tab />
+    <compare-tab />
   </c-simple-grid>
 </template>
 1
