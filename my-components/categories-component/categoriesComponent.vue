171728e7f27069d9b0ff35e52fc12bed7dd8bbcb
Abdulvohid Karimov <abdulvohid.karimjonov.97@gmail.com>
2022-05-10 11:37:29 +0500

    product slider backend connect

--- my-components/categories-component/categoriesComponent.vue
+++ my-components/categories-component/categoriesComponent.vue
@@ -1,19 +1,19 @@
 <script>
-  import { CThemeProvider, CSimpleGrid, CBox } from '@chakra-ui/vue'
-  import CategoryComponent from '~/my-components/categories-component/categoryComponent.vue'
-  import ViewAll from '~/library/viewAll.vue'
-  export default {
-    name: 'CategoriesComponent',
-    components: {
-      CategoryComponent,
-      CThemeProvider,
-      CSimpleGrid,
-      CBox,
-      ViewAll,
-    },
-    data() {
-      return {
-        categoryItem: [
+import { CThemeProvider, CSimpleGrid, CBox } from '@chakra-ui/vue'
+import CategoryComponent from '~/my-components/categories-component/categoryComponent.vue'
+import ViewAll from '~/library/viewAll.vue'
+export default {
+  name: 'CategoriesComponent',
+  components: {
+    CategoryComponent,
+    CThemeProvider,
+    CSimpleGrid,
+    CBox,
+    ViewAll,
+  },
+  data() {
+    return {
+      categoryItem: [
         { img: 'category1.png', name: 'Очистители и увлажнители воздуха' },
         { img: 'category2.png', name: 'Очистители и увлажнители воздуха' },
         { img: 'category3.png', name: 'Очистители и увлажнители воздуха' },
@@ -26,48 +26,48 @@
         { img: 'category10.png', name: 'Очистители и увлажнители воздуха' },
         { img: 'category11.png', name: 'Очистители и увлажнители воздуха' },
         { img: 'category12.png', name: 'Очистители и увлажнители воздуха' },
-        ],
-      }
-    },
-  }
+      ],
+    }
+  },
+}
 </script>
-
 <template>
   <CThemeProvider>
-    <CBox mt="88px" :mb="['30px', '41px', '52px', '64px', '76px', '88px']">
+    <CBox :mb="['30px', '41px', '52px', '64px', '76px', '88px']">
       <CFlex
-      align="center"
-      justify="space-between"
-      :mb="['10px', '15px', '20px', '25px', '30px', '40px']"
+        align="center"
+        justify="space-between"
+        :mb="['10px', '15px', '20px', '25px', '30px', '40px']"
       >
-      <h1 class="main_header">{{ $t('category') }}</h1>
-      <ViewAll />
-    </CFlex>
+        <h1 class="main_header">{{ $t('category') }}</h1>
+        <ViewAll />
+      </CFlex>
 
-    <CSimpleGrid
-    :columns="['4', '4', '4', '4', '4', '6']"
-    rows="2"
-    spacing-x="30"
-    :spacing-y="['18px', '22px', '26px', '30px', '34px', '40px']"
-    justify-items="center"
-    justify-content="center"
-    >
-    <CBox
-    class="category__component"
-    >
-    <CategoryComponent v-for="(item, idx) in categoryItem" :key="idx" :item="item" />
-  </CBox>
-</CSimpleGrid>
-</CBox>
-</CThemeProvider>
+      <CSimpleGrid
+        :columns="['4', '4', '4', '4', '4', '6']"
+        rows="2"
+        spacing-x="30"
+        :spacing-y="['18px', '22px', '26px', '30px', '34px', '40px']"
+        justify-items="center"
+        justify-content="center"
+      >
+        <CBox
+          v-for="(item, idx) in categoryItem"
+          :key="idx"
+          class="category__component"
+        >
+          <CategoryComponent :item="item" />
+        </CBox>
+      </CSimpleGrid>
+    </CBox>
+  </CThemeProvider>
 </template>
-
 <style lang="scss" scoped>
-  .category__component {
-    @media screen and(max-width: 62em) {
-      &:nth-child(n + 9) {
-        display: none;
-      }
+.category__component {
+  @media screen and(max-width: 62em) {
+    &:nth-child(n + 9) {
+      display: none;
     }
   }
+}
 </style>
