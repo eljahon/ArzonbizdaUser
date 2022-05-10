000a4637ffbaadfa91bb9769b8df8e1387224eb7
Muslihiddin <muslihiddin97@gmail.com>
2022-05-10 11:25:15 +0500

    Copare-page-done

--- pages/compare/index.vue
+++ pages/compare/index.vue
@@ -1,16 +1,23 @@
 <script>
 import { CSimpleGrid } from '@chakra-ui/vue'
 import CompareProduct from '~/my-components/comparing-component/CompareProduct.vue'
+import CompareProductMobile from '~/my-components/comparing-component/CompareProductMobile.vue'
 
 export default {
   name: 'ComparePage',
-  components: { CompareProduct, CSimpleGrid },
+  components: { CompareProduct, CSimpleGrid, CompareProductMobile },
 }
 </script>
 <template>
-  <c-simple-grid :columns="2" :spacing="'30px'" mt="40px">
+  <c-simple-grid
+    :columns="2"
+    :spacing="['10px', '12px', '16px', '30px', '30px', '30px']"
+    mt="40px"
+  >
     <compare-product />
     <compare-product />
-    
+    <compare-product-mobile />
+    <compare-product-mobile />
   </c-simple-grid>
 </template>
+1
