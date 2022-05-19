36a593c774a8c110c734fedaa55e6222cc12e384
Abdulvohid Karimov <abdulvohid.karimjonov.97@gmail.com>
2022-05-19 18:54:51 +0500

    finish AOS

--- pages/category.vue
+++ pages/category.vue
@@ -1,15 +1,34 @@
 <script>
 import CategoryMain from '~/my-components/categories-component/CategoryMain.vue'
+
+import aos from '@/helpers/aos'
+
+import AOS from '@/node_modules/aos/dist/aos'
+import 'aos/dist/aos.css'
+
 export default {
   name: 'UserComponent',
+
   components: {
     CategoryMain,
+  },
+
+  mixins: [
+    {
+      methods: {
+        aos,
+      },
+    },
+  ],
+
+  mounted() {
+    AOS.init({})
   },
 }
 </script>
 
 <template>
   <div>
-    <CategoryMain />
+    <CategoryMain data-aos="fade-up" data-aos-duration="1000" />
   </div>
 </template>
--- pages/search.vue
+++ pages/search.vue
@@ -1,17 +1,33 @@
 <script>
 import popularProducts from '~/my-components/popular-products/popularProducts.vue'
 import LoaderComponent from '~/library/LoaderComponent.vue'
+import aos from '@/helpers/aos'
+
+import AOS from '@/node_modules/aos/dist/aos'
+import 'aos/dist/aos.css'
 
 export default {
   name: 'SearchComponent',
   components: { popularProducts, LoaderComponent },
+
+  mixins: [
+    {
+      methods: {
+        aos,
+      },
+    },
+  ],
+
+  mounted() {
+    AOS.init({})
+  },
 }
 </script>
 
 <template>
   <div>
     <LoaderComponent />
-    <popular-products title="Найденные данные" />
+    <popular-products data-aos="fade-up" title="Найденные данные" />
   </div>
 </template>
 
