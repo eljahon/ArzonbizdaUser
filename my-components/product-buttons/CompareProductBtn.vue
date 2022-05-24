25104cedcd69bf60cf35936fc425f3e754a0ea42
Muslihiddin <muslihiddin97@gmail.com>
2022-05-24 17:37:30 +0500

    buttons-separated-toComponents

---
+++ my-components/product-buttons/CompareProductBtn.vue
@@ -0,0 +1,31 @@
+<script>
+import { CButton, CIcon } from '@chakra-ui/vue'
+export default {
+  name: 'CompareProductBtn',
+  components: {
+    CIcon,
+    CButton,
+  },
+}
+</script>
+<template>
+  <c-button
+    height="64px"
+    :disabled="isBadgeLength === 2"
+    max-w="337px"
+    w="100%"
+    border="1px"
+    border-color="color.BorderColor"
+    rounded="12px"
+    bg="white"
+    :_hover="false"
+    :_active="false"
+    :_focus="false"
+    font-weight="400"
+    color="color.BorderColor"
+    @click="IsComponents"
+  >
+    <c-icon name="compare" size="24px" mr="10px" />
+    {{ $t('compare') }}
+  </c-button>
+</template>
---
+++ my-components/product-buttons/OpenProductBtn.vue
@@ -0,0 +1,28 @@
+<script>
+import { CButton } from '@chakra-ui/vue'
+export default {
+  name: 'OpenProductBtn',
+  components: {
+    CButton,
+  },
+}
+</script>
+<template>
+  <c-button
+    height="64px"
+    max-w="337px"
+    w="100%"
+    mr="30px"
+    border="1px"
+    border-color="color.BorderColor"
+    rounded="12px"
+    bg="color.BorderColor"
+    :_hover="false"
+    :_active="false"
+    :_focus="false"
+    font-weight="400"
+    color="white"
+  >
+    {{ $t('view') }}
+  </c-button>
+</template>
